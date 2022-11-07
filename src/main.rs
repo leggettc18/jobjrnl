2b084bc6652ba2e0980946a98f9268a355e31941
Christopher Leggett <chris@leggett.dev>
2022-11-07 16:08:55 -0500

    foramatting 🧹

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
-    let app = jobjrnl::JobApplication::new(String::from("Test Job"), String::from("2022-11-06"), true);
-    println!("{}, date applied: {}, resume sent: {}", app.name, app.date_applied, app.resume_sent)
+    let app =
+        jobjrnl::JobApplication::new(String::from("Test Job"), String::from("2022-11-06"), true);
+    println!(
+        "{}, date applied: {}, resume sent: {}",
+        app.name, app.date_applied, app.resume_sent
+    )
 }
