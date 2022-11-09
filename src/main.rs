fafd32a39e8736474b5eb675b10e88511bbbd63d
Christopher Leggett <chris@leggett.dev>
2022-11-09 10:28:24 -0500

    Removes unneeded pubs 🪥

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
     let app =
         jobjrnl::JobApplication::new(String::from("Test Job"), String::from("2022-11-06"), true);
-    println!(
-        "{}, date applied: {}, resume sent: {}",
-        app.name, app.date_applied, app.resume_sent
-    )
+    println!("{:?}", app)
 }
