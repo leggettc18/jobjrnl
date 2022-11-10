451ef5ed71286079f8ad4ae7094f96114edbf5ff
Christopher Leggett <chris@leggett.dev>
2022-11-10 09:16:24 -0500

    Adds more fields to JobApplication

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,31 @@
 #[derive(Debug)]
 pub struct JobApplication {
-    pub name: String,
-    pub date_applied: String,
-    pub resume_sent: bool,
+    name: String,
+    date_applied: String,
+    resume_sent: bool,
+    cover_letter_sent: bool,
+    response_received: Option<String>,
+    interview_date: Option<String>,
 }
 
 impl JobApplication {
-    pub fn new(name: String, date_applied: String, resume_sent: bool) -> Self { Self { name, date_applied, resume_sent } }
+    pub fn new(
+        name: String,
+        date_applied: String,
+        resume_sent: bool,
+        cover_letter_sent: bool,
+        response_received: Option<String>,
+        interview_date: Option<String>,
+    ) -> Self {
+        Self {
+            name,
+            date_applied,
+            resume_sent,
+            cover_letter_sent,
+            response_received,
+            interview_date,
+        }
+    }
 }
 
 pub fn add(left: usize, right: usize) -> usize {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    let app =
-        jobjrnl::JobApplication::new(String::from("Test Job"), String::from("2022-11-06"), true);
+    let app = jobjrnl::JobApplication::new(
+        String::from("Test Job"),
+        String::from("2022-11-10"),
+        true,
+        false,
+        None,
+        None,
+    );
     println!("{:?}", app)
 }
