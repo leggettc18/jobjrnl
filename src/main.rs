8ed7f33db655b6837b73f4580d9af9c02bf2c444
Christopher Leggett <chris@leggett.dev>
2022-11-13 16:32:21 -0500

    Adds command line parsing 🔍

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,69 @@
 use chrono::Local;
 use chrono::TimeZone;
+use clap::Args;
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    New(NewCmd),
+}
+
+#[derive(Args)]
+struct NewCmd {
+    name: String,
+    date: Option<String>,
+
+    #[arg(long)]
+    resume_sent: bool,
+
+    #[arg(long)]
+    coverletter_sent: bool,
+
+    #[arg(long)]
+    response_date: Option<String>,
+
+    #[arg(long)]
+    interview_date: Option<String>,
+}
 
 fn main() {
-    let app = jobjrnl::JobApplication::new(
-        String::from("Test Job"),
-        Local
-            .datetime_from_str("2022-11-10 12:00:00", "%Y-%m-%d %H:%M:%S")
-            .unwrap(),
-        true,
-        false,
-        None,
-        None,
-    );
-    println!("{}", app)
+    let cli = Cli::parse();
+
+    match cli.command {
+        Some(Commands::New(cmd)) => {
+            let app = jobjrnl::JobApplication::new(
+                cmd.name,
+                cmd.date
+                    .as_ref()
+                    .map(|date| {
+                        Local
+                            .datetime_from_str(date.as_ref(), "%Y-%m-%d %H:%M:%S")
+                            .unwrap()
+                    })
+                    .unwrap_or_else(Local::now),
+                cmd.resume_sent,
+                cmd.coverletter_sent,
+                cmd.response_date.as_ref().map(|date| {
+                    Local
+                        .datetime_from_str(date.as_ref(), "%Y-%m-%d %H:%M:%S")
+                        .unwrap()
+                }),
+                cmd.response_date.map(|date| {
+                    Local
+                        .datetime_from_str(date.as_ref(), "%Y-%m-%d %H:%M:%S")
+                        .unwrap()
+                }),
+            );
+            println!("{}", app)
+        }
+        None => {}
+    }
 }
